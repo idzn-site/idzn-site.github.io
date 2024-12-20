e06e8674ac1194774c80334f073e884668d6ef94
idzn-site <hery@idzn.site>
2024-12-20 21:47:43 +0300

    config_astrojs
    added deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from "@astrojs/sitemap";
 export default defineConfig({
+   site: 'https://idzn-site.github.io',
    vite: {
     plugins: [tailwindcss()],
   },
