8af5adef438c4243027c2622f26f0b7001f73181
idzn-site <hery@idzn.site>
2024-12-21 18:53:42 +0300

    astroplate-theme
    Trying anstroplate

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,54 @@
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import AutoImport from "astro-auto-import";
+import { defineConfig, squooshImageService } from "astro/config";
+import remarkCollapse from "remark-collapse";
+import remarkToc from "remark-toc";
+import config from "./src/config/config.json";
+
+// https://astro.build/config
 export default defineConfig({
-   site: 'https://idzn-site.github.io',
-   vite: {
-    plugins: [tailwindcss()],
+  site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
+  base: config.site.base_path ? config.site.base_path : "/",
+  trailingSlash: config.site.trailing_slash ? "always" : "never",
+  image: {
+    service: squooshImageService(),
   },
-  // add yur domain name here
-  site: 'https://lexingtonthemes.com',
-  compressHTML: true,
-  integrations: [sitemap()]
+  integrations: [
+    react(),
+    sitemap(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    AutoImport({
+      imports: [
+        "@/shortcodes/Button",
+        "@/shortcodes/Accordion",
+        "@/shortcodes/Notice",
+        "@/shortcodes/Video",
+        "@/shortcodes/Youtube",
+        "@/shortcodes/Tabs",
+        "@/shortcodes/Tab",
+      ],
+    }),
+    mdx(),
+  ],
+  markdown: {
+    remarkPlugins: [
+      remarkToc,
+      [
+        remarkCollapse,
+        {
+          test: "Table of contents",
+        },
+      ],
+    ],
+    shikiConfig: {
+      theme: "one-dark-pro",
+      wrap: true,
+    },
+    extendDefaultPlugins: true,
+  },
 });
