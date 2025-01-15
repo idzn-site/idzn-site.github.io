e8cadbffdc37af5040aed4f78dd5f1e641f30759
idzn-site <hery@idzn.site>
2025-01-15 22:17:19 +0300

    Contenu en français

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from "@astrojs/sitemap";
 export default defineConfig({
-   site: 'https://idzn-site.github.io',
    vite: {
     plugins: [tailwindcss()],
   },
